47d2286e68a8fae9a38e25b85c8c0c7e96d8782c
Willian <wcamacho221@gmail.com>
2022-04-22 18:00:15 -0500

    Base backend proyecto

---
+++ src/main/java/com/project/botapp/BotAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.botapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotAppApplication.class, args);
+	}
+
+}
